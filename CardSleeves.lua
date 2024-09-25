3c0663e4d7ccadad51067e8d8a403ef8270bd50f
Lars <lars.wijntjes@gmail.com>
2024-09-25 18:46:23 +0200

    Add sleeves to collection counter

--- CardSleeves.lua
+++ CardSleeves.lua
@@ -5,7 +5,7 @@
 --- MOD_AUTHOR: [LarsWijn]
 --- MOD_DESCRIPTION: Adds sleeves as modifier to decks. Art by Sable.
 --- PREFIX: casl
---- VERSION: 1.4.0-dev8
+--- VERSION: 1.4.0-dev9
 --- PRIORITY: -1
 --- DEPENDS: [Steamodded>=1.0.0~ALPHA-0924a]
 
