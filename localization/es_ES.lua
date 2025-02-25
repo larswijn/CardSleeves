6e1ec6ddcc428a79bb708b743b21e102108f3b96
Lars <lars.wijntjes@gmail.com>
2025-02-25 13:44:34 +0100

    Spanish localization

--- localization/es_ES.lua
+++ localization/es_ES.lua
@@ -1,6 +1,22 @@
 -- Spanish translation by Batolol and Yamper
 return {
     descriptions = {
+        Mod = {
+            CardSleeves = {
+                name = "Funda de Cartas",
+                text = {
+                    "{s:1.3}Añade {s:1.3,C:attention}15{} {s:1.3,E:1,C:dark_edition}Fundas{} {s:1.3}como modificadores de barajas.",
+                    " ",
+                    "Incluye una API para otros mods",
+                    "para añadir sus propias fundas.",
+                    " ",
+                    "Programación e implementación por {C:blue}Larswijn{}.",
+                    "Idea y arte original por {C:red}Sable{}.",
+                    " ",
+                    "{s:1.1}Vea https://github.com/larswijn/CardSleeves para mas información."
+                }
+            }
+        },
         Sleeve = {
             sleeve_casl_none = {
                 name = "Sin funda",
@@ -20,15 +36,38 @@ return {
                 name = "Funda Roja",
                 text = G.localization.descriptions.Back["b_red"].text
             },
+            sleeve_casl_red_alt = {
+                name = "Funda Roja",
+                text = {
+                    "{C:red}+#1#{} descarte en cada ronda",
+                    "",
+                    "{C:blue}#2#{} mano en cada ronda"
+                },
+            },
 
             sleeve_casl_blue = {
                 name = "Funda Azul",
                 text = G.localization.descriptions.Back["b_blue"].text
             },
+            sleeve_casl_blue_alt = {
+                name = "Funda Azul",
+                text = {
+                    "{C:blue}+#1#{} mano en cada ronda",
+                    "",
+                    "{C:red}#2#{} descarte en cada ronda"
+                },
+            },
 
             sleeve_casl_yellow = {
                 name = "Funda Amarilla",
                 text = G.localization.descriptions.Back["b_yellow"].text
+            },
+            sleeve_casl_yellow_alt = {
+                name = "Funda Amarilla",
+                text = {
+                    "Comienza la partida con el vale",
+                    "{C:money,T:v_seed_money}#1#{}"
+                },
             },
 
             sleeve_casl_green = {
@@ -38,7 +77,16 @@ return {
                     "+{C:money}$#1#{s:0.85} por cada {C:blue}Mano restante",
                     "+{C:money}$#2#{s:0.85} por cada {C:red}Descarte restante",
                     "No ganas {C:attention}Interes"
-                    }
+                }
+            },
+            sleeve_casl_green_alt = {
+                name = "Funda Verde",
+                text = {
+                    "Adquiere hasta",
+                    "{C:red}-$#1#{} en deuda por",
+                    "cada mano y descarte ",
+                    "{C:inactive}(Currently {C:red}-$#2#{C:inactive})"
+                }
             },
 
             sleeve_casl_black = {
@@ -46,12 +94,12 @@ return {
                 text = G.localization.descriptions.Back["b_black"].text
             },
             sleeve_casl_black_alt = {
-                name = "Funda negra",
+                name = "Funda Negra",
                 text = {
                     "{C:attention}+#1#{} Ranura de comodín",
                     "",
                     "{C:red}-#2#{} descartes",
-                    "en cada ronda"
+                    "cada ronda"
                 }
             },
 
@@ -63,7 +111,7 @@ return {
                 name = "Funda Mágica",
                 text = {
                     "Empieza la partida con",
-                    " el vale {C:tarot,T:v_omen_globe}#1#{}",
+                    "el vale {C:tarot,T:v_omen_globe}#1#{}",
                 }
             },
 
@@ -84,7 +132,7 @@ return {
                 text = G.localization.descriptions.Back["b_ghost"].text
             },
             sleeve_casl_ghost_alt = {
-                name = "Funda Fantasma",
+                name = "Funda Fastasma",
                 text = {
                     "la aparición de cartas {C:spectral}espectrales{}",
                     "en la tienda se dobla,",
@@ -98,7 +146,7 @@ return {
                 text = G.localization.descriptions.Back["b_abandoned"].text
             },
             sleeve_casl_abandoned_alt = {
-                name = "Funda abandonada",
+                name = "Funda Abandonada",
                 text = {
                     "Las cartas {C:attention}Figuras{} no",
                     "aparecerán durante la partida"
@@ -134,6 +182,13 @@ return {
             sleeve_casl_painted = {
                 name = "Funda Pintada",
                 text = G.localization.descriptions.Back["b_painted"].text
+            },
+            sleeve_casl_painted_alt = {
+                name = "Funda Pintada",
+                text = {
+                    "{C:attention}+#1#{} Límite de selección de carta,",
+                    "{C:red}#2#{} Ranuras de comodín",
+                }
             },
 
             sleeve_casl_anaglyph = {
@@ -178,6 +233,7 @@ return {
     misc = {
         dictionary = {
             k_sleeve = "Funda",
+            k_sleeves = "Fundas",
             gald_sleeves = "Seleccionar Funda",
             gald_random_sleeve = "Funda Aleatoria",
             sleeve_unique_effect_desc = "Algunas fundas tienen efectos únicos cuando se combinan con barajas específicas",
